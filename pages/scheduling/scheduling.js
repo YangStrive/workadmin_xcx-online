0f2e5d84c4564127d4fee4b3c0e1a1f33d353cee
杨帆 <>
2024-11-13 23:22:06 +0800

    feat:新增排班页面

--- pages/scheduling/scheduling.js
+++ pages/scheduling/scheduling.js
@@ -6,7 +6,102 @@ Page({
 	 */
 	data: {
 		showClickGrid: false,
+		current: 0,
+		user:[{
+			name: '张三',
+			firstName: '张',
+		}],
+		week: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+		scheduling: [
+			[
+				{
+					day: '周一',
+					date: '2021-08-23',
+					startTime: '08:00',
+					endTime: '12:00',
+					rest: false,
+				},
+				{
+					day: '周二',
+					date: '2021-08-23',
+					startTime: '14:00',
+					endTime: '18:00',
+					rest: false,
+				},
+				{
+					day: '周三',
+					date: '2021-08-23',
+					startTime: '19:00',
+					endTime: '21:00',
+					rest: false,
+				},
+				{
+					day: '周四',
+					date: '2021-08-23',
+					startTime: '21:00',
+					endTime: '23:00',
+					rest: false,
+				},
+				{
+					day: '周五',
+					date: '2021-08-23',
+					startTime: '08:00',
+					endTime: '12:00',
+					rest: false,
+				},
+				{
+					day: '周六',
+					date: '2021-08-23',
+					startTime: '08:00',
+					endTime: '12:00',
+					rest: false,
+				},
+				{
+					day: '周日',
+					date: '2021-08-23',
+					startTime: '08:00',
+					endTime: '12:00',
+					rest: false,
+				},
+			],
+		]
+			
+	},
 
+	init() {
+		//生成20个user
+		const user = []
+		for (let i = 0; i < 20; i++) {
+			user.push({
+				name: '张三' + i,
+				firstName: '张',
+			})
+		}
+		this.setData({
+			user
+		})
+
+		//生成20个scheduling
+		const scheduling = []
+		for (let i = 0; i < 20; i++) {
+			const item = []
+			for (let j = 0; j < 7; j++) {
+				item.push({
+					day: '周一',
+					date: '2021-08-23',
+					startTime: '08:00',
+					endTime: '12:00',
+					rest: false,
+				})
+			}
+			scheduling.push(item)
+		}
+		this.setData({
+			scheduling
+		})
+
+
+		console.log(scheduling)
 	},
 
 	handleClickGridScheduling() {
@@ -16,11 +111,19 @@ Page({
 		})
 	},
 
+	handleHeadSwiperChange(e) {
+		console.log(e)
+	},
+
+	handleBodySwiperChange(e) {
+		console.log(e)
+	},
+
 	/**
 	 * 生命周期函数--监听页面加载
 	 */
 	onLoad(options) {
-
+		this.init()
 	},
 
 	/**
