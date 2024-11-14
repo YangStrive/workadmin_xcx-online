801a25b67bd01dadedeccc60ed179f15b7ba12c7
yangfan09 <yangfan09@huohua.cn>
2024-11-14 18:27:03 +0800

    no message

--- pages/scheduling/scheduling.js
+++ pages/scheduling/scheduling.js
@@ -12,63 +12,24 @@ Page({
 			firstName: '张',
 		}],
 		week: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
-		scheduling: [
-			[
-				{
-					day: '周一',
-					date: '2021-08-23',
-					startTime: '08:00',
-					endTime: '12:00',
-					rest: false,
-				},
-				{
-					day: '周二',
-					date: '2021-08-23',
-					startTime: '14:00',
-					endTime: '18:00',
-					rest: false,
-				},
-				{
-					day: '周三',
-					date: '2021-08-23',
-					startTime: '19:00',
-					endTime: '21:00',
-					rest: false,
-				},
-				{
-					day: '周四',
-					date: '2021-08-23',
-					startTime: '21:00',
-					endTime: '23:00',
-					rest: false,
-				},
-				{
-					day: '周五',
-					date: '2021-08-23',
-					startTime: '08:00',
-					endTime: '12:00',
-					rest: false,
-				},
-				{
-					day: '周六',
-					date: '2021-08-23',
-					startTime: '08:00',
-					endTime: '12:00',
-					rest: false,
-				},
-				{
-					day: '周日',
-					date: '2021-08-23',
-					startTime: '08:00',
-					endTime: '12:00',
-					rest: false,
-				},
-			],
-		]
-			
+		tableBodyScheduling: [],
+		swiperHeadList: [],
+		//今天的日期字符串
+		nowDateStr: '',
+		headerCurrent: 1,
+		bodyCurrent: 1,
 	},
 
 	init() {
+		const now = new Date()
+		const nowDate = now.getDate()
+		const nowMonth = now.getMonth()
+		const nowYear = now.getFullYear()
+		const nowDateStr = `${nowYear}-${nowMonth + 1}-${nowDate}`
+		this.setData({
+			nowDateStr
+		})
+
 		//生成20个user
 		const user = []
 		for (let i = 0; i < 20; i++) {
@@ -81,42 +42,152 @@ Page({
 			user
 		})
 
-		//生成20个scheduling
-		const scheduling = []
+		this.initSwiperHeadList()
+		this.getSchedulingDetail(1)
+
+	},
+
+	//生成头部swiper数据
+	initSwiperHeadList() {
+		const swiperHeadList = this.generateWeekData(new Date())
+		this.setData({
+			swiperHeadList
+		})
+	},
+
+	//生成当前日期的当前周，上一周，下一周的数据
+	generateWeekData(date) {
+		console.log(date, 666)
+		const weekData = [];
+		const dayMilliseconds = 24 * 60 * 60 * 1000;
+		const weekMilliseconds = 7 * dayMilliseconds;
+		const weekDays = ['周一', '周二', '周三', '周四', '周五', '周六', '周日'];
+	
+		const getWeekArray = (startDate) => {
+			const weekArray = [];
+			for (let i = 0; i < 7; i++) {
+				const currentDate = new Date(startDate.getTime() + i * dayMilliseconds);
+				weekArray.push({
+					date: weekDays[currentDate.getDay() === 0 ? 6 : currentDate.getDay() - 1],
+					day: currentDate.getDate(),
+					datestr: `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`,
+					month: currentDate.getMonth() + 1
+				});
+			}
+			return weekArray;
+		};
+	
+		const inputDate = new Date(date);
+		const currentDay = inputDate.getDay();
+		const startOfWeek = new Date(inputDate.getTime() - ((currentDay === 0 ? 7 : currentDay) - 1) * dayMilliseconds);
+	
+		// 上一周
+		weekData.push(getWeekArray(new Date(startOfWeek.getTime() - weekMilliseconds)));
+		// 当前周
+		weekData.push(getWeekArray(startOfWeek));
+		// 下一周
+		weekData.push(getWeekArray(new Date(startOfWeek.getTime() + weekMilliseconds)));
+		console.log(weekData,888)
+	
+		return weekData;
+	},
+
+	//点击排班格子
+	handleClickGridScheduling() {
+		this.setData({
+			showClickGrid: true,
+		})
+	},
+
+	//头部swiper切换
+	handleHeadSwiperChange(e) {
+		let current = e.detail.current;
+		console.log('handleHeadSwiperChange', current)
+		//当前current第一天的日期
+		const firstDate = this.data.swiperHeadList[current][0].datestr;
+		console.log(firstDate, 999)
+		//重新生成头部swiper数据
+		let swiperHeadList = this.generateWeekData(new Date(firstDate));
+
+		//如果current 为0,需要对swiperHeadList重新排序，把当前周放在第一个，下一周放在第二个，上一周放在第三个
+		if(current === 0) {
+			const temp = swiperHeadList[0]
+			swiperHeadList[0] = swiperHeadList[1];
+			swiperHeadList[1] = swiperHeadList[2];
+			swiperHeadList[2] = temp;
+		}
+		//如果current 为2,需要对swiperHeadList重新排序，把当前周放在第三个，下一周放在第一个，上一周放在第二个
+		if (current === 2) {
+			const temp = swiperHeadList[2]
+			swiperHeadList[2] = swiperHeadList[1];
+			swiperHeadList[1] = swiperHeadList[0];
+			swiperHeadList[0] = temp;
+		}
+		
+		this.setData({
+			swiperHeadList,
+			bodyCurrent: current
+		})
+	},
+
+	//body swiper切换
+	handleBodySwiperChange(e) {
+		console.log('handleBodySwiperChange')
+		this.setData({
+			headerCurrent: e.detail.current
+		});
+		this.getSchedulingDetail(e.detail.current)
+		
+	},
+
+	//获取详细排班数据
+	getSchedulingDetail(current) {
+		//随机生成二十个排班数据，数据中对象的字段带上默认值
+		const scheduling = [];
+		let startTimeList = ['08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00','15:00','16:00','17:00'];
+		let endTimeList = ['09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00','16:00','17:00','18:00'];
 		for (let i = 0; i < 20; i++) {
 			const item = []
 			for (let j = 0; j < 7; j++) {
+				//给随机生成的排班数据添加默认值
+				const day = Math.floor(Math.random() * 30) + 1;
+				const date = `${this.data.swiperHeadList[this.data.bodyCurrent][j].month}-${day}`;
+				const startTime = startTimeList[Math.floor(Math.random() * 10)];
+				const endTime = endTimeList[Math.floor(Math.random() * 10)];
+				const rest = false;
 				item.push({
-					day: '周一',
-					date: '2021-08-23',
-					startTime: '08:00',
-					endTime: '12:00',
-					rest: false,
+					day,
+					date,
+					startTime,
+					endTime,
+					rest,
 				})
 			}
 			scheduling.push(item)
 		}
-		this.setData({
-			scheduling
-		})
+		//生成20个空scheduling
+		const schedulingEmpty = []
+		for (let i = 0; i < 20; i++) {
+			const item = []
+			for (let j = 0; j < 7; j++) {
+				item.push({
+					day: '',
+					date: '',
+					startTime: '',
+					endTime: '',
+					rest: false,
+				})
+			}
+			schedulingEmpty.push(item)
+		}
 
-
-		console.log(scheduling)
-	},
-
-	handleClickGridScheduling() {
-		console.log(99)
-		this.setData({
-			showClickGrid: true
-		})
-	},
-
-	handleHeadSwiperChange(e) {
-		console.log(e)
-	},
-
-	handleBodySwiperChange(e) {
-		console.log(e)
+		let tableBodyScheduling = [schedulingEmpty, schedulingEmpty, schedulingEmpty]
+		tableBodyScheduling[current] = scheduling;
+		setTimeout(() => {
+			this.setData({
+				tableBodyScheduling
+			})
+		}, 500)
 	},
 
 	/**
