ecaeade7e40cdc466d480bf064f31878d30c1279
yangfan09 <yangfan09@huohua.cn>
2024-11-18 14:46:49 +0800

    no message

--- pages/scheduling/scheduling.js
+++ pages/scheduling/scheduling.js
@@ -3,6 +3,7 @@ import {schedule, shift} from './sched.js'
 import {
 	generateWeekData,
 	analysisuserList,
+	debounce
 } from './util.js'
 Page({
 
@@ -30,14 +31,18 @@ Page({
 		//临时排班
 		temporarySchedulingList:  [
 		],
-		showAddClickGridMain: true,
+		showAddClickGridMain: false,//是否显示添加排班，true显示，false不显示，默认显示
 		showAddScheedulingMain: false,
 		userLength:0,
 		selectedGridNum:0,
 		showDeleteClickGridMain: false,
+		selectedGridIndexList:{},//选中的排班索引
+		currentSchedleFirstDate:'',
+		schedulingEmpty:[],//空排班数据
 	},
 
 	async init() {
+		this.getSchedulingDetail = debounce(this.getSchedulingDetailDeb, 1000)
 		const now = new Date()
 		const nowDate = now.getDate()
 		const nowMonth = now.getMonth()
@@ -51,14 +56,14 @@ Page({
 
 		let data = await this.getSchedulingData();
 		let userList = this.setUserList(data);
-		let swiperHeadList = this.initSwiperHeadList();
-		let tableBodyScheduling = await this.getSchedulingDetail(1,data)
+		this.setEmptySchedulingData(data);
+		let swiperHeadList = this.initSwiperHeadList(data);
+		this.getSchedulingDetail(1,data)
 		
 		this.setData({
 			userList,
 			userLength:userList.length,
 			swiperHeadList,
-			tableBodyScheduling,
 			fixedSchedulingList:shift,
 		})
 
@@ -81,68 +86,98 @@ Page({
 	},
 
 	//生成头部swiper数据
-	initSwiperHeadList() {
-		const swiperHeadList = generateWeekData(new Date())
+	initSwiperHeadList(data) {
+		let firstDate = data[0].date_schedule[0].date;
+		const swiperHeadList = generateWeekData(new Date(firstDate*1000));
 		return swiperHeadList;
 	},
 
 	//生成表格中用户姓名信息
 	setUserList(data){
 		return analysisuserList(data);
+	},
+
+	//点击批量排班
+	handleBatchScheduleBtn(){
+		this.setData({
+			showAddClickGridMain: true,
+			showClickGrid: true,
+		})
+	},
+
+	//批量排班
+	handleBatchSchedulePro(e){
+		const schedulenum = e.currentTarget.dataset.schedulenum;
+		let selectedGridNum = this.data.selectedGridNum;
+
+		//如果schedulenum大于0则不做任何操作
+		if(schedulenum > 0 ){
+			return;
+		}
+
+		this.handleSelectGridScheduling(e);
 
 	},
 
-	//点击排班格子
-	handleClickGridScheduling(e) {
+	//公共方法处理选择和反选格子
+	handleSelectGridScheduling(e){
 		const userId = e.currentTarget.dataset.userid;
 		const date = e.currentTarget.dataset.date;
 		const dateIndex = e.currentTarget.dataset.dateindex;
 		const userIndex = e.currentTarget.dataset.userindex;
-		const tableBodyScheduling = this.data.tableBodyScheduling;
-		const schedulenum = e.currentTarget.dataset.schedulenum;
+		let tableBodyScheduling = this.data.tableBodyScheduling;
+		let selectedGridIndexList = this.data.selectedGridIndexList;
 		let selectedGridNum = this.data.selectedGridNum;
-
-		//如果showAddClickGridMain为true,并且schedulenum大于0,则不允许点击
-		if(this.data.showAddClickGridMain && schedulenum > 0){
-			return;
-		}
-
-		//schedulenum大于0则需要显示当前排班信息
-		if(schedulenum > 0 && !this.data.showAddClickGridMain){
-			const selectSchedulingList = tableBodyScheduling[this.data.bodyCurrent][userIndex].date_schedule[dateIndex].schedule;
-			this.setData({
-				selectSchedulingList,
-				showAddClickGridMain:false,
-				showAddScheedulingMain:true,
-			})
-			return;
-		}
 
 		//如果selected为true,则取消选中，否则选中
 		if (tableBodyScheduling[this.data.bodyCurrent][userIndex].date_schedule[dateIndex].selected) {
 			tableBodyScheduling[this.data.bodyCurrent][userIndex].date_schedule[dateIndex].selected = false;
 			selectedGridNum--;
+			//将取消选中的排班索引从selectedGridIndexList中删除
+			selectedGridIndexList[this.data.currentSchedleFirstDate] = selectedGridIndexList[this.data.currentSchedleFirstDate].filter(item => {
+				return item != `${userIndex}-${dateIndex}`
+			})
 		}else {
 			tableBodyScheduling[this.data.bodyCurrent][userIndex].date_schedule[dateIndex].selected = true;
 			selectedGridNum++;
+			selectedGridIndexList[this.data.currentSchedleFirstDate].push(`${userIndex}-${dateIndex}`)
 		}
-
-
 		this.setData({
-			showClickGrid: true,
 			tableBodyScheduling,
 			selectedGridNum
 		})
 	},
 
+	//点击排班格子
+	handleClickGridScheduling(e) {
+		const schedulenum = e.currentTarget.dataset.schedulenum;
+		
+		//如果是删除状态
+		if(this.data.showDeleteClickGridMain){
+			this.handleDeleteGridScheduling(e);
+			return;
+		}
+
+		if(this.data.showAddClickGridMain){
+			this.handleBatchSchedulePro(e);
+			return;
+		}
+
+		//schedulenum大于0则需要显示当前排班信息
+		if(schedulenum > 0 ){
+		}
+	},
+
 	//头部swiper切换
-	handleHeadSwiperChange(e) {
+	async handleHeadSwiperChange(e) {
 		let current = e.detail.current;
 		console.log('handleHeadSwiperChange', current)
 		//当前current第一天的日期
 		const firstDate = this.data.swiperHeadList[current][0].datestr;
 		const headSelectYear = firstDate.split('-')[0];
 		const headSelectMonth = firstDate.split('-')[1];
+		let schedulingEmpty = this.data.schedulingEmpty
+		
 		console.log(firstDate, 999)
 		//重新生成头部swiper数据
 		let swiperHeadList = generateWeekData(new Date(firstDate));
@@ -167,35 +202,33 @@ Page({
 			bodyCurrent: current,
 			headSelectMonth,
 			headSelectYear,
+			tableBodyScheduling: [schedulingEmpty,schedulingEmpty,schedulingEmpty],
 		})
+
+
+		this.getSchedulingDetail(current)
 	},
 
 	//body swiper切换
-	handleBodySwiperChange(e) {
+	async handleBodySwiperChange(e) {
 		console.log('handleBodySwiperChange')
+		let schedulingEmpty = this.data.schedulingEmpty
 		this.setData({
-			headerCurrent: e.detail.current
+			headerCurrent: e.detail.current,
+			tableBodyScheduling: [schedulingEmpty,schedulingEmpty,schedulingEmpty],
 		});
 		this.getSchedulingDetail(e.detail.current)
 		
 	},
 
-	//获取详细排班数据
-	async getSchedulingDetail(current,data) {
-		//随机生成userLength个空排班数据，数据中对象的字段带上默认值
-		let scheduleData = []
-		if(data){
-			scheduleData = data;
-		}else{
-			scheduleData = await this.getSchedulingData();
-		}
-
+	//生成空格子数据
+	setEmptySchedulingData(data){
 		let schedulingEmpty = [];
-		const userLength = scheduleData.length;
+		const userLength = data.length;
 		for (let i = 0; i < userLength; i++) {
 			let item = {
-				user_id: scheduleData[i].user_id,
-				user_name: scheduleData[i].user_name,
+				user_id: data[i].user_id,
+				user_name: data[i].user_name,
 				date_schedule:[],
 			}
 			for (let j = 0; j < 7; j++) {
@@ -225,25 +258,66 @@ Page({
 			schedulingEmpty.push(item)
 		}
 
+		this.setData({
+			schedulingEmpty,
+		})
+	},
+
+	//设置详细排班数据
+	async getSchedulingDetailDeb(current,data) {
+		//随机生成userLength个空排班数据，数据中对象的字段带上默认值
+		let scheduleData = []
+		if(data){
+			scheduleData = data;
+		}else{
+			scheduleData = await this.getSchedulingData();
+		}
+
 		//将scheduleData中date_schedule字段增加一个selected字段，用于标记是否被选中
 		scheduleData.forEach((item, index) => {
 			item.date_schedule.forEach((dateItem, dateIndex) => {
 				dateItem.selected = false;
 			})
 		})
-		
-		
 
+		//当前排班第一天的日期
+		let currentSchedleFirstDate = scheduleData[0].date_schedule[0].date;
+		let selectedGridIndexList = this.data.selectedGridIndexList;
+
+		if(!selectedGridIndexList[currentSchedleFirstDate]){
+			selectedGridIndexList[currentSchedleFirstDate] = [];
+		}
+
+		//如果之前已经选择了排班，需要将已选择的排班选中
+		if(selectedGridIndexList[currentSchedleFirstDate].length > 0){
+			selectedGridIndexList[currentSchedleFirstDate].map(item => {
+				let userIndex = item.split('-')[0];
+				let dateIndex = item.split('-')[1];
+				//选中已选择的排班
+				scheduleData[userIndex].date_schedule[dateIndex].selected = true
+			})
+		}
+
+		this.setData({
+			currentSchedleFirstDate,
+			selectedGridIndexList
+		})
+
+		let schedulingEmpty = this.data.schedulingEmpty;
 		let tableBodyScheduling = [schedulingEmpty, schedulingEmpty, schedulingEmpty]
 		tableBodyScheduling[current] = scheduleData;
 		console.log('getSchedulingDetail', tableBodyScheduling)
 
-		return tableBodyScheduling;
+		this.setData({
+			tableBodyScheduling,
+		})
 	},
 
 	//点击取消排班
 	handleCancelScheduleBtn() {
 		const tableBodyScheduling = this.data.tableBodyScheduling;
+		let selectedGridIndexList = this.data.selectedGridIndexList;
+		selectedGridIndexList[this.data.currentSchedleFirstDate] = [];
 
 		//取消所有选中的排班
 		tableBodyScheduling[this.data.bodyCurrent].forEach((item, index) => {
@@ -256,10 +330,9 @@ Page({
 			showClickGrid: false,
 			selectedGridNum:0,
 			tableBodyScheduling,
+			selectedGridIndexList,
 		})
 	},
-
-
 
 	//点击添加排班
 	handleClickAddScheduleBtn() {
@@ -269,8 +342,6 @@ Page({
 			showAddScheedulingMain: true,
 		})
 	},
-
-
 
 	//删除已选择的排班
 	handleClickDeleteSeletedScheduleBtn(e) {
@@ -318,6 +389,9 @@ Page({
 
 	},
 
+	/**
+	 * 删除已存在的排班逻辑
+	 */
 
 	//点击批量删除排班
 	handleClickAllDeleteBtn(){
@@ -329,13 +403,36 @@ Page({
 
 	},
 
+	//删除排班格子
+	handleDeleteGridScheduling(e){
+		const schedulenum = e.currentTarget.dataset.schedulenum;
+		if(schedulenum == 0 ){
+			return;
+		}
+		this.handleSelectGridScheduling(e);
+	},
+
+	//点击取消批量删除排班
 	handleDeleteCancelScheduleBtn(){
+		const tableBodyScheduling = this.data.tableBodyScheduling;
+		let selectedGridIndexList = this.data.selectedGridIndexList;
+		selectedGridIndexList[this.data.currentSchedleFirstDate] = [];
+		//取消所有选中的排班
+		tableBodyScheduling[this.data.bodyCurrent].forEach((item, index) => {
+			item.date_schedule.forEach((dateItem, dateIndex) => {
+				dateItem.selected = false;
+			})
+		})
+
 		this.setData({
 			showDeleteClickGridMain:false,
 			selectedGridNum:0,
+			tableBodyScheduling,
+			selectedGridIndexList,
 		})
 	},
 
+	//点击确定批量删除排班
 	handleDeleteCanfirmBtn(){
 
 	},
